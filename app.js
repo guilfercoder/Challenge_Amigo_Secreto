cbcc1841efec8a3185e07a06849cdf816ea6539d
Guilherme Ferreira <guilherme-53@hotmail.com>
2025-02-24 19:50:07 -0300

    função para adicionar amigos na pesquisa

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+
+function adicionarAmigos(){
+    let campoNomes = document.getElementById("nomeAmigos");
+    let nome = campoNomes.ariaValueMax.trim();
+
+    if (nome === ""){
+        alert("Digite um nome válido.");
+        return;
+    }
+}
